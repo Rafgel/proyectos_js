d17cdd724080665257fe4e10054e74e61068c791
Rafgel <114894870+Rafgel@users.noreply.github.com>
2023-04-12 18:49:41 -0600

    guardado_2

--- mat.js
+++ mat.js
@@ -16,3 +16,36 @@ function dividir(x){
 
 
 exports.dividir = dividir;
+
+
+// ejercicio #14
+
+function sonAnagramas(cadena1, cadena2) {
+  
+    if (cadena1.length !== cadena2.length) {
+      return false;
+    }
+  
+
+    var arreglo1 = cadena1.split('').sort();
+    var arreglo2 = cadena2.split('').sort();
+  
+  
+    for (let i = 0; i < arreglo1.length; i++) {
+      if (arreglo1[i] !== arreglo2[i]) {
+        return false;
+      }
+    }
+  
+    
+    return true;
+  }
+  
+ 
+  const cadena1 = 'jabon';
+  const cadena2 = 'banjo';
+  if (sonAnagramas(cadena1, cadena2)) {
+    console.log(`${cadena1} y ${cadena2} son anagramas`);
+  } else {
+    console.log(`${cadena1} y ${cadena2} no son anagramas`);
+  }
